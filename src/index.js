1e924a98b9d71ae87854c0693f87b16569e467e2
Stephen Bannasch <stephen.bannasch@gmail.com>
2021-11-22 11:49:03 -0500

    connect draft spyglass page into main carousel

--- src/index.js
+++ src/index.js
@@ -4,33 +4,78 @@ let selectRight = document.getElementById('select-right');
 let selectLeft = document.getElementById('select-left');
 let selectedImage = document.getElementById('selected-image');
 
-let images = [
-  "images/thoreau-snowshoes-close.png",
-  "images/spyglass.png",
-  "images/lock-and-key.png",
-  "images/desk.png",
-  "images/flutes.png",
-  "images/walking-stick.png"
+let carouselImageContainer = document.getElementById('carousel-image-container');
+
+let pages = [{
+    location: "snowshoes.html",
+    enabled: false,
+    src: "images/thoreau-snowshoes-close.png"
+  },
+  {
+    location: "spyglass.html",
+    enabled: true,
+    src: "images/spyglass.png"
+  },
+  {
+    location: "lock-and-key.html",
+    enabled: false,
+    src: "images/lock-and-key.png"
+  },
+  {
+    location: "desk.html",
+    enabled: false,
+    src: "images/desk.png"
+  },
+  {
+    location: "flute.html",
+    enabled: false,
+    src: "images/flutes.png"
+  },
+  {
+    location: "walking-stick.html",
+    enabled: false,
+    src: "images/walking-stick.png"
+  }
 ]
 
-let index = 0;
+let pageIndex = 0;
+let selectedPage = pages[pageIndex];
 
-let nextImage = () => {
-  index += 1;
-  if (index >= images.length) index = 0;
+let updateSelectedPage = () => {
+  if (selectedPage.enabled) {
+    selectedImage.classList.add('enabled');
+  } else {
+    selectedImage.classList.remove('enabled');
+  }
+  selectedImage.src = selectedPage.src;
 }
 
-let previousImage = () => {
-  index -= 1;
-  if (index > 0) index = images.length - 1;
+let nextPage = () => {
+  pageIndex += 1;
+  if (pageIndex >= pages.length) pageIndex = 0;
+  selectedPage = pages[pageIndex];
+  updateSelectedPage();
+}
+
+let previousPage = () => {
+  pageIndex -= 1;
+  if (pageIndex > 0) pageIndex = pages.length - 1;
+  selectedPage = pages[pageIndex];
+  updateSelectedPage();
 }
 
 selectRight.addEventListener('click', () => {
-  nextImage();
-  selectedImage.src = images[index];
+  nextPage();
 })
 
 selectLeft.addEventListener('click', () => {
-  previousImage();
-  selectedImage.src = images[index];
+  previousPage();
 })
+
+carouselImageContainer.addEventListener('click', () => {
+  if (selectedPage.enabled) {
+    location = selectedPage.location;
+  }
+})
+
+updateSelectedPage();
