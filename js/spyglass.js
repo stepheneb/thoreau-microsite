288791508e185e6f987f887e586cfae64946cf9d
stepheneb <stepheneb@users.noreply.github.com>
2022-02-10 18:35:34 +0000

    deploy: bb755ec32734284e8a76976e95a52d0c8894c6ff

--- js/spyglass.js
+++ js/spyglass.js
@@ -7,10 +7,19 @@ import { startup } from './modules/artifact.js';
 let animations = [{
     imgPrefix: 'spyglass/Spyglass_PNGSeq_01__',
     startFrame: 46,
-    endFrame: 160,
+    endFrame: 77,
     startScroll: 1.95,
-    endScroll: 4.5,
+    endScroll: 2.8,
     startPage: 1,
+    endPage: 2,
+  },
+  {
+    imgPrefix: 'spyglass/Spyglass_PNGSeq_01__',
+    startFrame: 78,
+    endFrame: 160,
+    startScroll: 3,
+    endScroll: 4.5,
+    startPage: 3,
     endPage: 4,
   },
   {
