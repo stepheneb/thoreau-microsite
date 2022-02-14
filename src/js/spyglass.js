23ff529830af8f21a8909485aeffca25a8c3b5ca
Stephen Bannasch <stephen.bannasch@gmail.com>
2022-02-14 16:31:07 -0500

    spyglass: better matching to motion-comp

--- src/js/spyglass.js
+++ src/js/spyglass.js
@@ -7,20 +7,29 @@ import { startup } from './modules/artifact.js';
 let animations = [{
     imgPrefix: 'spyglass/spyglass_animals_10fps/Render_Spyglass+Animals_10_fps_',
     startFrame: 0,
-    endFrame: 144,
-    startScroll: 1.95,
-    endScroll: 4.5,
+    endFrame: 109,
+    startScroll: 1.9,
+    endScroll: 3.95,
     startPage: 1,
-    endPage: 4,
+    endPage: 3,
+  },
+  {
+    imgPrefix: 'spyglass/spyglass_animals_10fps/Render_Spyglass+Animals_10_fps_',
+    startFrame: 110,
+    endFrame: 144,
+    startScroll: 4,
+    endScroll: 5.25,
+    startPage: 4,
+    endPage: 5,
   },
   {
     imgPrefix: 'spyglass/trees_02_10fps/Render_Trees_10_fps_',
     startFrame: 0,
     endFrame: 99,
-    startScroll: 5.0,
-    endScroll: 8.5,
-    startPage: 5,
-    endPage: 9,
+    startScroll: 6.1,
+    endScroll: 7.5,
+    startPage: 6,
+    endPage: 11,
   }
 ];
 
