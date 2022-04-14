ed7b6b282df9096ca6299c97ad5ea3ae9af9c529
stepheneb <stepheneb@users.noreply.github.com>
2022-04-14 20:48:17 +0000

    deploy: d5a28d156a5ff25a905d782b568f10d12814c7cc

---
+++ js/desk.js
@@ -0,0 +1,23 @@
+/*jshint esversion: 8 */
+/*global  */
+
+import { app } from "./modules/globals.js"
+import { startup } from './modules/artifact.js';
+
+let animations = [{
+  imgPrefix: 'desk/Journal_',
+  startFrame: 0,
+  endFrame: 128,
+  startScroll: 2.0,
+  endScroll: 6.95,
+  startPage: 2,
+  endPage: 7,
+}];
+
+app.maxContentScroll = 12;
+
+app.dev = true;
+
+app.domReady(() => {
+  startup('desk', animations);
+});
