b12fb1c034437201222b0c28137af210fbc5e272
stepheneb <stepheneb@users.noreply.github.com>
2022-04-15 18:04:39 +0000

    deploy: 293d8c2c1157cd25d981e1e8f22b0abd462285ea

--- js/lock-and-key.js
+++ js/lock-and-key.js
@@ -5,14 +5,35 @@ import { app } from "./modules/globals.js"
 import { startup } from './modules/artifact.js';
 
 let animations = [{
-  imgPrefix: 'lock-and-key/Coin+Jail+Town_',
-  startFrame: 0,
-  endFrame: 253,
-  startScroll: 2.0,
-  endScroll: 7.95,
-  startPage: 2,
-  endPage: 8,
-}];
+    imgPrefix: 'lock-and-key/Coin+Jail+Town_',
+    startFrame: 0,
+    peakFrame: 45,
+    endFrame: 101,
+    startScroll: 2.1,
+    endScroll: 4.98,
+    startPage: 2,
+    endPage: 5,
+  },
+  {
+    imgPrefix: 'lock-and-key/Coin+Jail+Town_',
+    startFrame: 102,
+    peakFrame: 142,
+    endFrame: 176,
+    startScroll: 5.1,
+    endScroll: 7.98,
+    startPage: 5,
+    endPage: 8,
+  },
+  {
+    imgPrefix: 'lock-and-key/Coin+Jail+Town_',
+    startFrame: 177,
+    endFrame: 254,
+    startScroll: 8.1,
+    endScroll: 10.8,
+    startPage: 8,
+    endPage: 11,
+  }
+];
 
 app.maxContentScroll = 12;
 
