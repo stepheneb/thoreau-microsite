b33612d0e94bc93bbad9f9dedd5d7335acdb4a66
Stephen Bannasch <stephen.bannasch@gmail.com>
2022-05-19 18:07:27 -0400

    utility: nextRepaint

--- src/js/modules/utilities.js
+++ src/js/modules/utilities.js
@@ -9,3 +9,18 @@ export const u = {};
 u.deepClone = (obj) => {
   return JSON.parse(JSON.stringify(obj));
 };
+
+u.nextRepaint = () => {
+  return new Promise(resolve => {
+    window.requestAnimationFrame(function () {
+      window.requestAnimationFrame(function () {
+        resolve();
+      });
+    });
+  });
+}
+
+// async function asyncCall() {
+//   await nextRepaint();
+//  ...
+// }
